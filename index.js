8fbb5fedb68988ba4287bab040e335cfdaf0aa20
mellonges <mellonges@mellongess-iMac.local>
2022-04-12 20:22:09 +0300

    fix

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-// import chalk from "chalk";
+import chalk from "chalk";
 
 
-// const regExp = /(\d+(\.\d+)?)\s*(s|m|h|D|M|Y)/
+const regExp = /(\d+(\.\d+)?)\s*(s|m|h|D|M|Y)/
 
-// let v = 1
-//
-// const keysValues = {
-//     s: v *= 1000,
-//     m: v *= 60,
-//     h: v *= 60,
-//     D: v *= 24,
-//     M: v *= 30,
-//     Y: v *= 12
-// }
-//
-// const keysDels = {
-//     s: 1 / keysValues.s,
-//     m: 1 / keysValues.m,
-//     h: 1 / keysValues.h,
-//     D: 1 / keysValues.D,
-//     M: 1 / keysValues.M,
-//     Y: 1 / keysValues.Y,
-// }
+let v = 1
 
-//  function strToTime(str = '') {
-//     let sum = 0
+const keysValues = {
+    s: v *= 1000,
+    m: v *= 60,
+    h: v *= 60,
+    D: v *= 24,
+    M: v *= 30,
+    Y: v *= 12
+}
+
+const keysDels = {
+    s: 1 / keysValues.s,
+    m: 1 / keysValues.m,
+    h: 1 / keysValues.h,
+    D: 1 / keysValues.D,
+    M: 1 / keysValues.M,
+    Y: 1 / keysValues.Y,
+}
+
+ function strToTime(str = '') {
+    let sum = 0
+
+    /** @type {RegExpExecArray} */
+    let array = null
+
+    while (array = regExp.exec(str)) {
+        const [find, ...args] = array
+        const num = args.shift()
+        const key = args.pop()
+
+        str = str.replace(find, '')
+        sum += +num * (keysValues[key] || 0)
+    }
+
+    return sum
+}
 //
-//     /** @type {RegExpExecArray} */
-//     let array = null
-//
-//     while (array = regExp.exec(str)) {
-//         const [find, ...args] = array
-//         const num = args.shift()
-//         const key = args.pop()
-//
-//         str = str.replace(find, '')
-//         sum += +num * (keysValues[key] || 0)
-//     }
-//
-//     return sum
-// }
-//
-// const time = strToTime('2D 1D 32h 12m')
+const time = strToTime('2D 1D 32h 12m')
 
 
  export function timeStampToDate(time) {
@@ -82,4 +82,4 @@
     )
 }
 
-// console.log(`unix time: ${chalk.red(strToTime(process.argv[2]))} \n format time: ${chalk.yellowBright(timeStampToDate(strToTime(process.argv[2])))}`)
+console.log(`unix time: ${chalk.red(strToTime(process.argv[2]))} \n format time: ${chalk.yellowBright(timeStampToDate(strToTime(process.argv[2])))}`)
